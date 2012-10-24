ee51b5f5198f1f7724bd92fea9f5a3ecc4f885a2
Ross Riley <riley.ross@gmail.com>
2012-10-24 20:06:37 +0100

    phpunit config file

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
 spl_autoload_register(function ($class) {
-  die($class);
   if (0 === strpos(ltrim($class, '/'), 'Wax\Db')) {
     if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Wax\Db')).'.php')) {
       require_once $file;
