9bfc652f98ca5da1d28442eedfda450c56d48393
Ross Riley <riley.ross@gmail.com>
2012-10-24 20:14:51 +0100

    debug bootstrap running

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+die("Got to bootstrap");
 spl_autoload_register(function ($class) {
   if (0 === strpos(ltrim($class, '/'), 'Wax\Db')) {
     if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Wax\Db')).'.php')) {
