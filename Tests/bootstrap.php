d165dbb5705cae803d295f7a458a3e1a24294e67
Ross Riley <riley.ross@gmail.com>
2012-10-24 20:01:08 +0100

    test of bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
+  die($class);
   if (0 === strpos(ltrim($class, '/'), 'Wax\Db')) {
     if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Wax\Db')).'.php')) {
       require_once $file;
